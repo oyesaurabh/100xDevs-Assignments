2ddd266533a4a799fb23ded4aac0ed73fc5ebea1
saurabh yadav <oye.saurabhyadav@gmail.com>
2023-10-31 20:05:23 +0530

    completed the authenticationServer

--- Week-2/02-nodejs/authenticationServer.js
+++ Week-2/02-nodejs/authenticationServer.js
@@ -14,47 +14,44 @@ const app = express();
 
 // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
 app.use(express.json());
-const users = [];
+let users = [];
 
 // 1. POST /signup - User Signup
-// Description: Allows users to create an account. This should be stored in an array on the server, and a unique id should be generated for every new user that is added.
+// Description: Allows users to create an account. This should be stored in an array on the server,
+// and a unique id should be generated for every new user that is added.
 // Request Body: JSON object with username, password, firstName and lastName fields.
 // Response: 201 Created if successful, or 400 Bad Request if the username already exists.
 // Example: POST http://localhost:3000/signup
-app.post("/signup ", (req, res) => {
-  const { username } = req.body;
-  const usernameExist = users.find((item) => item.username == username);
-  if (usernameExist) return res.status(400).send("ALREADY EXISTS");
+app.post("/signup", (req, res) => {
+  const { email } = req.body;
+  const userExist = users.find((item) => item.email == email);
+  if (userExist) return res.status(400).send("User Already Exists");
   users.push({
-    id: Math.round(Math.random() * 10000),
     ...req.body,
   });
-  res.status(201).send("CREATED");
+  res.status(201).send("Signup successful");
 });
-
 // 2. POST /login - User Login
 // Description: Gets user back their details like firstname, lastname and id
 // Request Body: JSON object with username and password fields.
 // Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
 // Example: POST http://localhost:3000/login
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  const user = [];
+  const { email, password } = req.body;
+  let user = null;
   for (let i = 0; i < users.length; i++) {
-    if (users[i].username == username) {
-      if (users[i].password != password)
-        return res.status(402).send("WRONG PASSWORD");
+    if (users[i].email == email && users[i].password == password) {
       user = users[i];
       break;
     }
   }
   user
-    ? res.status(201).send({
-        firstname: user.firstname,
-        lastname: user.lastname,
-        id: user.id,
+    ? res.send({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
       })
-    : res.status(401);
+    : res.status(401).send("USER NOT FOUND");
 });
 
 // 3. GET /data - Fetch all user's names and ids from the server (Protected route)
@@ -63,24 +60,28 @@ app.post("/login", (req, res) => {
 // Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
 // Example: GET http://localhost:3000/data
 app.get("/data", (req, res) => {
-  //first checking if we are authenticated or not
-  const user = req.body;
-  const isAuth = false;
+  const { username, password } = req.headers;
+  let isAuth = false;
   for (let i = 0; i < users.length; i++) {
-    if (
-      users[i].username == user.username &&
-      users[i].password == user.password
-    ) {
+    if (users[i].username == username && users[i].password == password) {
       isAuth = true;
       break;
     }
   }
-  if (!isAuth) return res.status(401);
-  res.send(users);
+  if (!isAuth) return res.status(401).send("Unauthorized");
+
+  const userData = users.map((user) => {
+    return {
+      firstname: user.firstname,
+      lastname: user.lastname,
+    };
+  });
+  res.send({ users: userData });
 });
 
 // - For any other route not defined in the server return 404
 app.all("*", (req, res) => {
   res.status(404).send("ROUTE NOT FOUND");
 });
+
 module.exports = app;
